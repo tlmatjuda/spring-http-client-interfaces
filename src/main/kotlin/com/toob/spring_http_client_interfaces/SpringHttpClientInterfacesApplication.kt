a740c9281537daae99fbe98ad9ab39771966b4c2
Thabo Matjuda <tl.matjuda@gmail.com>
2025-01-21 12:44:12 +0200

    Initial commit

---
+++ src/main/kotlin/com/toob/spring_http_client_interfaces/SpringHttpClientInterfacesApplication.kt
@@ -0,0 +1,11 @@
+package com.toob.spring_http_client_interfaces
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringHttpClientInterfacesApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringHttpClientInterfacesApplication>(*args)
+}
---
+++ src/test/kotlin/com/toob/spring_http_client_interfaces/SpringHttpClientInterfacesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toob.spring_http_client_interfaces
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringHttpClientInterfacesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
